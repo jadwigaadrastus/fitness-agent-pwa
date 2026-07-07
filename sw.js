ca77e0e4b213eb9c4ac663572bd133614023bb8c
jadwigaadrastus <jadwigaadrastus@outlook.com>
2026-07-07 18:50:04 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'fitness-agent-pwa-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request)));
+});
